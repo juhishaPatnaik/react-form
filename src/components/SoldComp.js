6e1445fa29b059b58b7c1b089bbf2181f0463912
juhishaPatnaik <34659094+juhishaPatnaik@users.noreply.github.com>
2018-03-21 12:55:25 +0530

    Update SoldComp.js

--- src/components/SoldComp.js
+++ src/components/SoldComp.js
@@ -2,38 +2,33 @@ import React, { Component } from 'react';
 
 class SoldComp extends Component {
     render() {
-        return ( <
-            div className = "grid-item" >
-            <
-            b > Sold To: < /b> <br/ >
-            <
-            div className = "FormGrp" >
-            <
-            label htmlFor = "businessID" > < span className = "Astric" > * < /span>Business ID:</label >
-            <
-            input className = "Input"
-            type = "text"
-            name = "businessID"
-            placeholder = "Business ID"
-            onChange = {
-                (e) => { this.props.handleChange(e) }
-            }
-            / > < /
-            div > <
-            div className = "FormGrp" >
-            <
-            label htmlFor = "soldBusinessName" > < span className = "Astric" > * < /span>Business Name:</label >
-            <
-            input className = "Input"
-            type = "text"
-            name = "soldBusinessName"
-            placeholder = "Business Name"
-            onChange = {
-                (e) => { this.props.handleChange(e) }
-            }
-            / > <
-            /div> < /
-            div >
+        return ( 
+        <div className = "grid-item">
+            <b> Sold To: </b>
+            <br/ >
+            <div className = "FormGrp" >
+                <label htmlFor = "businessID" > < span className = "Astric" > * </span>Business ID:</label>
+                <input className = "Input"
+                type = "text"
+                name = "businessID"
+                placeholder = "Business ID"
+                value={this.props.data.businessID}
+                onChange = {
+                    (e) => { this.props.handleChange(e) }
+                }/>
+            </div>
+            <div className = "FormGrp">
+                <label htmlFor = "soldBusinessName" >< span className = "Astric" > * </span>Business Name:</label>
+                <input className = "Input"
+                type = "text"
+                name = "soldBusinessName"
+                placeholder = "Business Name"
+                value={this.props.data.soldBusinessName}
+                onChange = {
+                    (e) => { this.props.handleChange(e) }
+                }/>
+            </div>
+        </div >
         )
     }
 }
