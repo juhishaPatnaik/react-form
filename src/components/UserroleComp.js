ba7e1ce27517ee09d822ddd3b2c8fe1c7720ac30
juhishaPatnaik <34659094+juhishaPatnaik@users.noreply.github.com>
2018-03-21 11:58:41 +0530

    Update UserroleComp.js

--- src/components/UserroleComp.js
+++ src/components/UserroleComp.js
@@ -13,45 +13,50 @@ class UserroleComp extends Component {
     CloseAndSave() {
         console.log('Close and save button');
     }
+    Engineering(){
+        console.log('Engineering button');
+    }
+    Hold(){
+        console.log('On Hold button');
+    }
+    Order(){
+        console.log('Order button');
+    }
+    Complete(){
+        console.log('Complete button');
+    }
     render() {
 
-        return ( <
-            div >
-            <
-            hr / >
-            Current User: < input type = "text" / > < input type = "text" / > Created By UserId: < input type = "text" / > < br / >
-            Created By Role: < input type = "text" / >
-            <
-            hr / >
-            <
-            input type = "text"
+        return ( 
+            <div>
+            <hr/>
+            Current User: < input type = "text" /> < input type = "text" /> Created By UserId: < input type = "text" / > < br / >
+            Created By Role: < input type = "text"/>
+            <hr/>
+            <input type = "text"
             className = "Input"
             style = {
                 { 'width': '100%' }
-            }
-            /> <
-            div className = "buttonDiv" >
-            <
-            button className = "btnapp"
-            onClick = { this.Close } > Close < /button> <
-            button className = "btnapp"
-            onClick = { this.CloseAndSave } > Save and Close < /button> <
-            button className = "btnapp"
-            onClick = {
+            }/>
+            <div className = "buttonDiv" >
+            <button className = "btnapp" onClick = { this.Close }> Close</button> 
+            <button className = "btnapp"
+            onClick = { this.CloseAndSave } > Save and Close </button> <
+            button className = "btnapp"  onClick = {
                 (e) => { this.Submit(e) }
-            } > Submit < /button> <
-            button className = "btnapp" > Enhineering < /button> <
-            button className = "btnapp" > On Hold < /button> <
-            button className = "btnapp" > Cancel Order < /button> <
-            button className = "btnapp" > Complete < /button> < /
-            div > <
-            br / >
-            Form Name: < input type = "text"
+            } > Submit </button> 
+            <button className = "btnapp" onClick = { this.Engineering } > Engineering </button>
+            <button className = "btnapp" onClick = { this.Hold } > On Hold </button>
+            <button className = "btnapp" onClick = { this.Order } > Cancel Order </button>
+            <button className = "btnapp" onClick = { this.Complete } > Complete </button>
+            </div>
+            <br/>
+            Form Name: <input type = "text"
             style = {
                 { "width": "20%" }
             }
-            /> < /
-            div >
+            />
+            </div >
         )
     }
 }
