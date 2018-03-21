ad55b4fd7f8c29e465ae9d9b150dd1875eb2d9c2
juhishaPatnaik <34659094+juhishaPatnaik@users.noreply.github.com>
2018-03-21 11:41:27 +0530

    Update ShipComp.js

--- src/components/ShipComp.js
+++ src/components/ShipComp.js
@@ -4,60 +4,49 @@ class ShipComp extends Component {
     render() {
         return (
           <div className = "grid-item" >
-            <b> Ship To: </b> <br/ >
-            <
-            div className = "FormGrp" >
-            <
-            label htmlFor = "shipBusinessName" > < span className = "Astric" > * < /span>Business Name:</label >
-            <
-            input className = "Input"
+            <b> Ship To: </b> <br/>
+            <div className = "FormGrp">
+            <label htmlFor = "shipBusinessName">< span className ="Astric" >*</span>Business Name:</label>
+            <input className = "Input"
             type = "text"
             name = "shipBusinessName"
             placeholder = "Business Name"
             onChange = {
                 (e) => { this.props.handleChange(e) }
-            }
-             requierd/ > < /
-            div > <
-            div className = "FormGrp" >
-            <
-            label htmlFor = "shipAddress" > < span className = "Astric" > * < /span>Address :</label >
-            <
-            input className = "Input"
+            }/>
+            </div>
+            <div className = "FormGrp">
+            <label htmlFor = "shipAddress" > < span className = "Astric" > * </span>Address :</label >
+            <input className = "Input"
             type = "text"
             name = "shipAddress"
             placeholder = "Address"
             onChange = {
                 (e) => { this.props.handleChange(e) }
-            }
-            / > < /
-            div > <
-            div className = "FormGrp" >
-            <
-            label htmlFor = "shipAddress1" > { ' ' } < /label> <
-            input className = "Input"
+            }/>
+            </div>
+            <div className = "FormGrp">
+            <label htmlFor = "shipAddress1" > { ' ' } </label>
+            <input className = "Input"
             type = "text"
             name = "shipAddress1"
             placeholder = "Address"
             onChange = {
                 (e) => { this.props.handleChange(e) }
-            }
-            / > < /
-            div > <
-            div className = "FormGrp" >
-            <
-            label htmlFor = "cityStateZip" > < span className = "Astric" > * < /span>City, State, Zip:</label >
-            <
-            input className = "Input"
+            }/>
+            </div >
+            <div className = "FormGrp">
+            <label htmlFor = "cityStateZip"> 
+                < span className = "Astric" > * </span>City, State, Zip:</label>
+            <nput className = "Input"
             type = "text"
             name = "cityStateZip"
             placeholder = "City, State, Zip"
             onChange = {
                 (e) => { this.props.handleChange(e) }
-            }
-            / > < /
-            div > < /
-            div >
+            }/> 
+            </div >
+            </div >
         )
     }
 }
